c49f58000dd2c4762dc45692c404e2afc378edcc
Filipe Araujo <filipe.alves.araujo@gmail.com>
2015-12-03 23:02:13 -0500

    adding service workers

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ var sass = require('gulp-sass');
 var debug = require('gulp-debug');
 var polybuild = require('polybuild');
 var exec = require('child_process').exec;
+var packageJson = require('./package.json');
+var fs = require('fs');
+var crypto = require('crypto');
+var glob = require('glob-all');
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 10',
@@ -200,7 +204,7 @@ gulp.task('default', ['clean'], function(cb) {
     ['copy', 'styles'],
     'elements',
     ['jshint', 'images', 'media', 'fonts', 'html'],
-    'vulcanize',
+    'vulcanize', 'cache:config',
     cb);
 });
 
@@ -224,6 +228,35 @@ gulp.task('deploy', function(cb) {
     cb(err);
   });
 });
+
+gulp.task('cache:config', function(callback) {
+  var dir = './dist';
+  var config = {
+    cacheId: packageJson.name || path.basename(__dirname),
+    disabled: false
+  };
+
+  glob([
+    'index.html',
+    'bower_components/webcomponentsjs/webcomponents.min.js',
+    'elements/podcast-app.build.html',
+    'elements/podcast-app.build.js'],
+    {cwd: dir}, function(error, files) {
+    if (error) {
+      callback(error);
+    } else {
+      config.precache = files;
+
+      var md5 = crypto.createHash('md5');
+      md5.update(JSON.stringify(config.precache));
+      config.precacheFingerprint = md5.digest('hex');
+
+      var configPath = path.join(dir, 'cache-config.json');
+      fs.writeFile(configPath, JSON.stringify(config, null, 4), callback);
+    }
+  });
+});
+
 
 // Load tasks for web-component-tester
 // Adds tasks for `gulp test:local` and `gulp test:remote`
